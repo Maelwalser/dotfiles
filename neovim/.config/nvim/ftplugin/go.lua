16c5de00b89c6b56133e05d22545d51f1a33ec24
Mael Walser <maelwalser04@gmail.com>
2026-01-19 09:24:14 +0100

    added some keybinds to golang dev environment

--- neovim/.config/nvim/ftplugin/go.lua
+++ neovim/.config/nvim/ftplugin/go.lua
@@ -29,3 +29,15 @@ vim.keymap.set("n", "<leader>gn", function() jump_to_error(1) end, { buffer = tr
 vim.keymap.set("n", "<leader>gp", function() jump_to_error(-1) end, { buffer = true, desc = "Go to previous Error" })
 
 vim.keymap.set({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { buffer = true, desc = "LSP Code Action" })
+
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = true, desc = "LSP Code Action" })
+
+vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, { buffer = true, desc = "LSP Code Action" })
+
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true, desc = "LSP Code Action" })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true, desc = "LSP Code Action" })
+
+vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, { buffer = true, desc = "LSP Code Action" })
+vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, { buffer = true, desc = "LSP Code Action" })
